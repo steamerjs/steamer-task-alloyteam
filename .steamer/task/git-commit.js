917c35187d6b246a63436da2f78586de1192c957
lcxfs1991 <lcxfs1991@gmail.com>
2017-12-07 22:36:35 +0800

    add jb id question

--- .steamer/task/git-commit.js
+++ .steamer/task/git-commit.js
@@ -1,40 +1,42 @@
 const path = require('path');
 
 module.exports = function(ctx, next) {
-
-    let jbConfig = require('../steamer-plugin-jb');
-    let questions = [
-        {
-            type: 'text',
-            message: 'Input your git commit  message',
-            name: 'msg',
-            default: 'update',
-        }
-    ];
-
-    if (!jbConfig.config.git.master) {
-        questions.push({
-            type: 'text',
-            message: 'Input your JB RID',
-            name: 'jb',
-            default: '',
-        });
-    }
-
-    let prompt = ctx.inquirer.createPromptModule();
-    prompt(questions).then((answers) => {
-
-        jbConfig.config.git.master = answers.jb;
-        ctx.fs.writeFileSync(path.join(process.cwd(), '.steamer/steamer-plugin-jb.js'), `module.exports = ${JSON.stringify(jbConfig, null, 4)};`, 'utf-8');
-
-        ctx.git(process.cwd())
-            .branchLocal((err, branches) => {
-                err && ctx.error(err);
-                ctx.currentBranch = branches.current;
-            })
-            .commit(`${answers.msg}`, (err) => {
-                err && ctx.error(err);
-                !err && next();
+    ctx.git(process.cwd())
+        .branchLocal((err, branches) => {
+            err && ctx.error(err);
+            ctx.currentBranch = branches.current;
+        })
+        .exec(() => {
+            let jbConfig = require('../steamer-plugin-jb');
+            let questions = [
+                {
+                    type: 'text',
+                    message: 'Input your git commit  message',
+                    name: 'msg',
+                    default: 'update',
+                }
+            ];
+        
+            if (!jbConfig.config.git[ctx.currentBranch]) {
+                questions.push({
+                    type: 'text',
+                    message: 'Input your JB RID',
+                    name: 'jb',
+                    default: '',
+                });
+            }
+        
+            let prompt = ctx.inquirer.createPromptModule();
+            prompt(questions).then((answers) => {
+        
+                jbConfig.config.git.master = answers.jb;
+                ctx.fs.writeFileSync(path.join(process.cwd(), '.steamer/steamer-plugin-jb.js'), `module.exports = ${JSON.stringify(jbConfig, null, 4)};`, 'utf-8');
+        
+                ctx.git(process.cwd())
+                    .commit(`${answers.msg}`, (err) => {
+                        err && ctx.error(err);
+                        !err && next();
+                    });
             });
-    });
+        });
 };
